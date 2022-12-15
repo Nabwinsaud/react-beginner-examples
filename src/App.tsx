baacda55f065a37d119d6c8782d41093d51f2104
Nabwinsaud <codinglogic1@gmail.com>
2022-12-15 15:23:39 +0545

    beginner project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import AccordionComponent from "./components/Accordion";
+import Record from "./components/users/Record";
 function App() {
   return (
     <div className="flex flex-col h-screen items-center justify-center w-full">
@@ -6,6 +7,7 @@ function App() {
       <h1 className="text-center my-2 uppercase text-pink-700 text-4xl ">
         Let build some beginner stuffs
       </h1>
+      <Record />
     </div>
   );
 }
--- src/components/users/Record.tsx
+++ src/components/users/Record.tsx
@@ -1,3 +1,46 @@
+import { useState } from "react";
+
+type User = {
+  name: string;
+  age: number;
+  email: string;
+};
+
 export default function Record() {
-  return <div>Record</div>;
+  const [user, setUser] = useState<Record<string, User>>({});
+
+  //   "aanaaja3"
+  const addUser = () => {
+    const id: string = Date.now().toString(36);
+    setUser({
+      ...user,
+      [id]: {
+        name: "nabin",
+        age: 19,
+        email: "nabin@gmail.com",
+      },
+    });
+  };
+  return (
+    <div className="flex flex-col">
+      <button
+        onClick={addUser}
+        className="rounded-md hover:cursor-pointer active:offset-2 active:offset-blue-600  bg-indigo-600 text-white uppercase py-2 px-6"
+      >
+        Add user
+      </button>
+
+      <div>
+        {Object.entries(user).map(([id, user]) => (
+          <div
+            key={id}
+            className="flex my-2 px-2  font-bold space-x-2  rounded-sm items-center bg-slate-400 text-white"
+          >
+            <p className="text-green-600">{id}</p>
+            <p className="text-blue-600 uppercase">{user.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
